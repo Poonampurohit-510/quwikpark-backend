f1d61033bd4ef96272df7d8573a68a920a8741a5
Poonampurohit-510 <22jr1a43d4cai@gmail.com>
2025-07-10 14:45:54 +0530

    Initial server setup

---
+++ bin/quwikpark_backend.dart
@@ -0,0 +1,5 @@
+import 'package:quwikpark_backend/quwikpark_backend.dart' as quwikpark_backend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${quwikpark_backend.calculate()}!');
+}
---
+++ test/quwikpark_backend_test.dart
@@ -0,0 +1,8 @@
+import 'package:quwikpark_backend/quwikpark_backend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
